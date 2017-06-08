78155b76762f3da316581adf807c7f674f53fa21
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-06-08 20:00:25 +0200

    Title and author added to content.opf

--- app/routes/generate-epub/generate-epub.index.js
+++ app/routes/generate-epub/generate-epub.index.js
@@ -1,30 +1,34 @@
-var archiver = require('archiver');
+const archiver = require('archiver');
 const ejs = require('ejs')
-
-var fileSystem = require('fs'),
-    path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 async function generateEpubFile(sections) {
     return sections
 }
 
 async function handler(ctx) {
-    const response = await generateEpubFile(ctx.request.body)
-    var filePath = path.join(__dirname, '../../../assets/empty-epub/mimetype');
-    var archive = archiver('zip', {
-        zlib: {level: 9} // Sets the compression level.
-    });
-    const articleContent = ejs.render(fileSystem.readFileSync(path.join(__dirname, '../../../assets/article.tmpl.ejs'), 'utf-8'), {
+    const { publisher, author, title } = ctx.request.body
+    const archive = archiver('zip', {
+        zlib: {level: 9}
+    })
+    const articleContent = ejs.render(fs.readFileSync(path.join(__dirname, '../../../assets/article.tmpl.xhtml'), 'utf-8'), {
         elements: ctx.request.body.elements
-    });
+    })
 
+    const contentOPFContent = ejs.render(fs.readFileSync(path.join(__dirname, '../../../assets/content.tmpl.opf'), 'utf-8'), {
+        publisher,
+        author,
+        title
+    })
     // ctx.body = archive
     ctx.body = archive
-    archive.append(articleContent, { name: 'OEBPS/article.xhtml' });
+    archive.append(articleContent, {name: 'OEBPS/article.xhtml'});
+    archive.append(contentOPFContent, {name: 'OEBPS/content.opf'});
     archive.directory(path.join(__dirname, '../../../assets/empty-epub/META-INF'), 'META-INF');
     archive.directory(path.join(__dirname, '../../../assets/empty-epub/OEBPS'), 'OEBPS');
     // archive.file(path.join(__dirname, '../../../assets/empty-epub/mimetype'), 'mimetype');
-    archive.append(fileSystem.createReadStream(path.join(__dirname, '../../../assets/empty-epub/mimetype')), { name: 'mimetype' });
+    archive.append(fs.createReadStream(path.join(__dirname, '../../../assets/empty-epub/mimetype')), {name: 'mimetype'});
     archive.finalize();
 
 }
